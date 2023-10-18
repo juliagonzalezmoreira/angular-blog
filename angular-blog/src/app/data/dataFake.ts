1bca4833a23c50c95b9536507f63d20c9930cd00
julia <juliagonzalezmoreira@gmail.com>
2023-10-18 08:42:35 -0300

    typescript

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+      "id":"1",
+      "title": "Blog com Angular!!!",
+      "description": "Meu primeiro projeto com Angular!",
+      "photoCover":"https://www.pngkey.com/png/detail/233-2332677_ega-png.png"
+    },
+    {
+      "id":"2",
+      "title": "Big Card II",
+      "description": "Segundo Card",
+      "photoCover":"https://www.pngkey.com/png/detail/233-2332677_ega-png.png"
+    }
+  ]
